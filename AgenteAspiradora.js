afb3ab560f91b52e426a727a92c9899e7270080e
SantiagoRuben <56168305+SantiagoRuben@users.noreply.github.com>
2021-03-25 20:39:18 -0600

    Primera actualizacion
    Se agregaron las funcione para colocar y eliminar a la aspiradora

--- AgenteAspiradora.js
+++ AgenteAspiradora.js
@@ -1,15 +1,15 @@
-
-// funcion para crear el tablero
+var posicionAspiradora; // variable global para conocer la posicion de la aspiradora 
+// funcion para crear el tablero y poner la aspiradora (solo se hace al cargar por primera vez la pagina)
 window.onload= function creaTablero(){
 
 	const dimension = 10;	//dimension del tablero 
 	const tablero = document.getElementById("tablero"); // se obtiene el div donde se dibujara el tablero
-	let NumeroDiv = 1	//se usara para nombrar el id de cada un de los cuadros para poder manipularlos mas facil despues
-	for(let i=0; i<dimension; i++){	//for para crear las filas
-		let fila = document.createElement("div");	// se crea el div de la fila
+	var NumeroDiv = 1	//se usara para nombrar el id de cada un de los cuadros para poder manipularlos mas facil despues
+	for(var i=0; i<dimension; i++){	//for para crear las filas
+		var fila = document.createElement("div");	// se crea el div de la fila
 		fila.className= ("fila" );	// se le agrega una clase 
-			for(let j=0; j<dimension; j++){	//for para agregar los cuadros pertenecientes a la columna
-			let cuadro = document.createElement("div"); // se crea el div del cuadro
+			for(var j=0; j<dimension; j++){	//for para agregar los cuadros pertenecientes a la columna
+			var cuadro = document.createElement("div"); // se crea el div del cuadro
 			cuadro.className= "cuadro";	// se le asigna una clase
 			cuadro.id=NumeroDiv;	// se le asigna id para mejor manejo
 			//cuadro.textContent=cuadro.getAttribute("id");	
@@ -18,58 +18,26 @@ window.onload= function creaTablero(){
 		}// fin for de los cuadros 
 		tablero.appendChild(fila);	// se añade la fila al div tablero
 	}// fin for de las filas
-	//alert(cuadro.getAttribute("class"));
+	
+	//agregar la imagen de la aspiradora
+
+	posicionAspiradora = Math.round(Math.random()*100); // Numero aleatorio para la posicion de inicial de la aspiradora
+	//alert(posicionAspiradora);
+	agregarAspiradora(posicionAspiradora);
 }	
-
-
-
-/*
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-        #tablero {
-            display: inline-block;
-            border:5px solid;
-        }
-        .fila {
-            background-color: #999;
-            display: table;
-        }
-        .recuadro {
-            width:50px;
-            height:50px;
-            float:left;
-        }
-        .fila:nth-child(odd) .recuadro:nth-child(even),
-        .fila:nth-child(even) .recuadro:nth-child(odd) {
-            background-color: white;
-        }
-    </style>
-</head>
- 
-<body>
- 
-    <div id="tablero"></div>
- 
-</body>
-</html>
- 
-<script>
-const tamano=8;
-const tablero=document.getElementById("tablero");
-for (let i=0; i<tamano; i++) {
- 
-    // creamos la fila
-    let fila=document.createElement("div");
-    fila.classList.add("fila")
-    for (let j=0; j<tamano; j++) {
- 
-        // creamos cada elemento de la fila
-        let div=document.createElement("div");
-        div.classList.add("recuadro")
-        fila.appendChild(div);
-    }
-    tablero.appendChild(fila);
+// Funcion para elimianr la aspiradora del lugar actual 
+function eliminarAspiradora(){
+	var img = document.getElementById("imgAspiradora");
+	document.getElementById(posicionAspiradora).removeChild(img);
 }
-</script>*/
+
+// Funcion para poner la aspiradora
+function agregarAspiradora(){
+	var imgAspiradora = document.createElement("img"); // se crea el elemento imagen 
+	//llenar la informacion necesaria de la imagen
+	imgAspiradora.src = './aspiradora.jpg';
+	imgAspiradora.id = 'imgAspiradora';
+	imgAspiradora.className = 'img';
+	//Termina el llenado de informacion
+	document.getElementById(posicionAspiradora).appendChild(imgAspiradora);	//agregamos el elemnto al div correspondiente
+}
